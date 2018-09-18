f13ef42738595a44b6e28b117a038ae27397b9ea
Patty <patty.fresonke@forumcomm.com>
2018-09-18 16:45:46 -0500

    udpate with mapbox tokens

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicGZyZXk2MTIiLCJhIjoiY2ptODh1djd3MGo3bTNxbnlvdnViODIxMyJ9.iFrhO0XzLzDoqszF4J1sUQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
